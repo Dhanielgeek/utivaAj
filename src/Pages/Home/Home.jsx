e19b08839e4e08ff7957732ce0ce845356abcb7f
Dhanielgeek <dhanielknightz46@gmail.com>
2023-04-06 15:48:57 +0100

    Router has been added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Classes from './Style.module.css'
+
+const Home = () => {
+  return (
+    <div className= {Classes.Holder}>hellooooo</div>
+  )
+}
+
+export default Home
